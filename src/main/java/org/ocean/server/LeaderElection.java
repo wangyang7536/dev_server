14ce210751d752e3ac1bb911004f12d4068814b7
wangyang7536 <wangyang7536@gmail.com>
2023-02-21 20:05:30 -0800

    Add election code

---
+++ src/main/java/org/ocean/server/LeaderElection.java
@@ -0,0 +1,14 @@
+package org.ocean.server;
+
+public class LeaderElection {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void connectToZookeeper() {
+
+    }
+}
